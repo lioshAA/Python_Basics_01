6b47175cdc93698c609b66be0dd86d08962ce9ae
Alex <alex.obukhov2011@gmail.com>
2022-08-09 13:39:36 +0300

    HM 9

--- HW_9.py
+++ HW_9.py
@@ -3,4 +3,20 @@
 #Повторити усі методи і функції над множинами, рядками і списками.
 
 
-person = {'Прізвище' - 'оцінка'}
+
+#d = {'Obukhov': 12}
+d = dict(Shkarupa = 4, Obukhov = 4, Shkulip = 2, Bondarenko = 3, Shevchenko = 5)
+d['Stepanenko'] = 1 #додавання елементу
+print(d)
+d['Stepanenko'] = 3 #зміна значення
+print(d)
+del d['Shkulip'] #видалення елемента
+print(d)
+print(d.keys())  #виведення ключів
+print(d.values()) #виведення значень
+print(d.items()) #виведення елементів
+toprates = {} #довідник учнів з високими оцінками
+for i in d:
+    if d[i] >= 4:
+        toprates[i] = d[i]
+print(toprates)
